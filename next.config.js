d05e0968e53cd610e8b3ee5ef28dd71d83e80299
Iahara <isharaktii@gmail.com>
2026-07-05 12:42:41 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+const createNextIntlPlugin = require('next-intl/plugin')();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: '**' }
+    ]
+  }
+};
+
+module.exports = createNextIntlPlugin(nextConfig);
